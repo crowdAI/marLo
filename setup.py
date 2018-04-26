3458771938ff1abadc1ace2032a6868b06d038f6
SP Mohanty <spmohanty91@gmail.com>
2018-04-26 05:12:04 +0200

    Remove linter complaints

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from setuptools import setup, find_packages
 
 setup(name='marlo',
       version='0.0.1',
-      description='Environments for Multi Agent Reinforcement Learning using MalmÖ',
-      long_description="""
-        A collection of OpenAI Gym-like environments for Multi Agent reinforcement learning tasks
-        on MineCraft using Project MalmÖ
+      description='Environments for Multi Agent Reinforcement Learning \
+      using MalmÖ',
+      long_description=""" A collection of OpenAI Gym-like environments \
+      for Multi Agent reinforcement learning tasks on MineCraft using \
+      Project MalmÖ
       """,
       url='https://github.com/spMohanty/marLo',
       author='S.P. Mohanty',
