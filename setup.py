8cd5c3b57c0193eca348a06023b9c28ae6c05940
SP Mohanty <spmohanty91@gmail.com>
2018-04-26 05:04:55 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(name='marlo',
+      version='0.0.1',
+      description='Environments for Multi Agent Reinforcement Learning using MalmÖ',
+      long_description="""
+        A collection of OpenAI Gym-like environments for Multi Agent reinforcement learning tasks
+        on MineCraft using Project MalmÖ
+      """,
+      url='https://github.com/spMohanty/marLo',
+      author='S.P. Mohanty',
+      author_email='spmohanty91@gmail.com',
+      license='MIT License',
+      packages=find_packages(),
+      zip_safe=False,
+      install_requires=['gym>=0.10.5']
+)
