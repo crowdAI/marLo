8840258b5852b67841256bc9be973e76526c082a
SP Mohanty <spmohanty91@gmail.com>
2018-04-26 08:02:34 +0200

    Look for mission spec folders and include them all

--- marlo/__init__.py
+++ marlo/__init__.py
@@ -1,16 +1,42 @@
 from gym.envs.registration import registry, register, make, spec
 import os
+import json
 import logging
 
 logger = logging.getLogger(__name__)
 
-register(
-    id='marlo-cliff_walking-v0',
-    entry_point='marlo.envs.marlo_base_env:MarloBaseEnv',
-    kwargs={'marlo_mission_spec': 'marlo-cliff_walking-v0'},
-    tags={'wrapper_config.TimeLimit.max_episode_steps': 1000},
-    reward_threshold=10**10,
-)
+"""
+Assume all folders inside the `marlo_mission_specs` directory represents
+a mission_spec
+"""
+mission_specs_dir = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)),
+    "..", "marlo_mission_specs")
+for mission_spec in os.listdir(mission_specs_dir):
+    config_path = os.path.join(
+                mission_specs_dir,
+                mission_spec,
+                "config.json"
+            )
+
+    mission_spec_path = os.path.join(
+                mission_specs_dir,
+                mission_spec,
+                "mission_spec.xml"
+            )
+
+    if os.path.exists(config_path) and os.path.exists(mission_spec_path):
+        config = json.loads(open(config_path).read())
+        env_name = config["env_name"]
+        # Register this as a Gym Environment
+        register(
+            id='{}'.format(env_name),
+            entry_point='marlo.envs.marlo_base_env:MarloBaseEnv',
+            kwargs={'marlo_mission_spec': '{}'.format(env_name)},
+            tags={'wrapper_config.TimeLimit.max_episode_steps': 1000},
+            reward_threshold=10**10,
+        )
+        # TODO : Set timelimit from MissionSpec
 
 """
 Set MALMO_XSD_PATH for Conda Environments
