ab6c279d6d6700ae376e43eaa0ed0bf7edac2c02
SP Mohanty <spmohanty91@gmail.com>
2018-07-23 18:26:22 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(name='marlo',
       version='0.0.1dev2',
       description='Marlo',
-      url='https://github.com/spMohanty/marlo',
+      url='https://github.com/crowdAI/marlo',
       author='S.P. Mohanty',
       author_email='sharada.mohanty@epfl.ch',
       license='MIT License',
