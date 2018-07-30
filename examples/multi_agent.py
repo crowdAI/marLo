a1a9652c362826abaf79cebb6e363244ffb421a4
SP Mohanty <spmohanty91@gmail.com>
2018-07-30 06:18:07 +0200

    Add multi_agent example

---
+++ examples/multi_agent.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# Please ensure that you have two Minecraft clients running on port 10000 and 
+# port 10001 by doing : 
+# $MALMO_MINECRAFT_ROOT/launchClient.sh -port 10000
+# $MALMO_MINECRAFT_ROOT/launchClient.sh -port 10001
+
+import marlo
+client_pool = [('127.0.0.1', 10000),('127.0.0.1', 10001)]
+join_tokens = marlo.make('MarLo-MazeRunner-v0',
+                          params={
+                            "client_pool": client_pool,
+                            "agent_names" :
+                              [
+                                "MarLo-Agent-0",
+                                "MarLo-Agent-1"
+                              ]
+                          })
+# As this is a two-agent scenario,
+# there will just two join tokens
+assert len(join_tokens) == 2
+
+@marlo.threaded
+def run_agent(join_token):
+    env = marlo.init(join_token)
+    observation = env.reset()
+    done = False
+    count = 0
+    while not done:
+        _action = env.action_space.sample()
+        obs, reward, done, info = env.step(_action)
+        print("reward:", reward)
+        print("done:", done)
+        print("info", info)
+    env.close()
+
+# Run agent-0
+run_agent(join_tokens[0])
+# Run agent-1
+run_agent(join_tokens[1])
