73ca39c8f7fd120758c10f6ad09eec3e825afdf4
SP Mohanty <spmohanty91@gmail.com>
2018-08-10 13:59:52 +0200

    Include xml templates in package

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(name='marlo',
-      version='0.0.1dev8',
+      version='0.0.1dev9',
       description='Marlo',
       url='https://github.com/crowdAI/marlo',
       author='S.P. Mohanty',
       author_email='sharada.mohanty@epfl.ch',
       license='MIT License',
       packages=find_packages(),
-      package_data={'': ['assets/*.xml']},
+      package_data = {
+        '':['envs/*/templates/*.xml']
+      },
       zip_safe=False,
       install_requires=['gym', 'jinja2'],
       dependency_links=[]
