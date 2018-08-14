6e53eb6b23900320880c29462bcd97f836a6ef6c
SP Mohanty <spmohanty91@gmail.com>
2018-08-14 15:54:15 +0200

    Rename Basic env as FindTheGoal

--- marlo/envs/Basic/__init__.py
+++ marlo/envs/Basic/__init__.py
@@ -8,7 +8,7 @@ def _register():
     # Version 0 of env 
     ##########################################
     gym.envs.registration.register(
-        id='MarLo-Basic-v0',
+        id='MarLo-FindTheGoal-v0',
         entry_point=MarloEnvBuilder,
         kwargs={
             "extra_params": {
