19225c2ca3a1444ab9223401b713aae7f4795c5a
AndKram <v-andkra@microsoft.com>
2018-10-02 11:05:13 +0100

    git command script

--- git_add.bat
+++ git_add.bat
@@ -1,32 +1,32 @@
-git add marlo\envs\MobchaseTrain1\__init__.py  marlo\envs\MobchaseTrain1\main.py   
-REM git add marlo\envs\MobchaseTrain1\templates\mission.xml
-git add marlo\envs\MobchaseTrain2\__init__.py  marlo\envs\MobchaseTrain2\main.py   
-REM git add marlo\envs\MobchaseTrain2\templates\mission.xml
-git add marlo\envs\MobchaseTrain3\__init__.py  marlo\envs\MobchaseTrain3\main.py   
-REM git add marlo\envs\MobchaseTrain3\templates\mission.xml
-git add marlo\envs\MobchaseTrain4\__init__.py  marlo\envs\MobchaseTrain4\main.py   
-REM git add marlo\envs\MobchaseTrain4\templates\mission.xml
-git add marlo\envs\MobchaseTrain5\__init__.py  marlo\envs\MobchaseTrain5\main.py   
-REM git add marlo\envs\MobchaseTrain5\templates\mission.xml
+REM git add marlo\envs\MobchaseTrain1\__init__.py  marlo\envs\MobchaseTrain1\main.py   
+git add marlo\envs\MobchaseTrain1\templates\mission.xml
+REM git add marlo\envs\MobchaseTrain2\__init__.py  marlo\envs\MobchaseTrain2\main.py   
+git add marlo\envs\MobchaseTrain2\templates\mission.xml
+REM git add marlo\envs\MobchaseTrain3\__init__.py  marlo\envs\MobchaseTrain3\main.py   
+git add marlo\envs\MobchaseTrain3\templates\mission.xml
+REM git add marlo\envs\MobchaseTrain4\__init__.py  marlo\envs\MobchaseTrain4\main.py   
+git add marlo\envs\MobchaseTrain4\templates\mission.xml
+REM git add marlo\envs\MobchaseTrain5\__init__.py  marlo\envs\MobchaseTrain5\main.py   
+git add marlo\envs\MobchaseTrain5\templates\mission.xml
 
-git add marlo\envs\BuildbattleTrain1\__init__.py  marlo\envs\BuildbattleTrain1\main.py  
-REM git add marlo\envs\BuildbattleTrain1\templates\mission.xml
-git add marlo\envs\BuildbattleTrain2\__init__.py  marlo\envs\BuildbattleTrain2\main.py  
-REM git add marlo\envs\BuildbattleTrain2\templates\mission.xml
-git add marlo\envs\BuildbattleTrain3\__init__.py  marlo\envs\BuildbattleTrain3\main.py  
-REM git add marlo\envs\BuildbattleTrain3\templates\mission.xml
-git add marlo\envs\BuildbattleTrain4\__init__.py  marlo\envs\BuildbattleTrain4\main.py   
-REM git add marlo\envs\BuildbattleTrain4\templates\mission.xml
-git add marlo\envs\BuildbattleTrain5\__init__.py  marlo\envs\BuildbattleTrain5\main.py   
-REM git add marlo\envs\BuildbattleTrain5\templates\mission.xml
+REM git add marlo\envs\BuildbattleTrain1\__init__.py  marlo\envs\BuildbattleTrain1\main.py  
+git add marlo\envs\BuildbattleTrain1\templates\mission.xml
+REM git add marlo\envs\BuildbattleTrain2\__init__.py  marlo\envs\BuildbattleTrain2\main.py  
+git add marlo\envs\BuildbattleTrain2\templates\mission.xml
+REM git add marlo\envs\BuildbattleTrain3\__init__.py  marlo\envs\BuildbattleTrain3\main.py  
+git add marlo\envs\BuildbattleTrain3\templates\mission.xml
+REM git add marlo\envs\BuildbattleTrain4\__init__.py  marlo\envs\BuildbattleTrain4\main.py   
+git add marlo\envs\BuildbattleTrain4\templates\mission.xml
+REM git add marlo\envs\BuildbattleTrain5\__init__.py  marlo\envs\BuildbattleTrain5\main.py   
+git add marlo\envs\BuildbattleTrain5\templates\mission.xml
 
-git add marlo\envs\TreasurehuntTrain1\__init__.py  marlo\envs\TreasurehuntTrain1\main.py   
-REM git add marlo\envs\TreasurehuntTrain1\templates\mission.xml
-git add marlo\envs\TreasurehuntTrain2\__init__.py  marlo\envs\TreasurehuntTrain2\main.py   
-REM git add marlo\envs\TreasurehuntTrain2\templates\mission.xml
-git add marlo\envs\TreasurehuntTrain3\__init__.py  marlo\envs\TreasurehuntTrain3\main.py   
-REM git add marlo\envs\TreasurehuntTrain3\templates\mission.xml
-git add marlo\envs\TreasurehuntTrain4\__init__.py  marlo\envs\TreasurehuntTrain4\main.py   
-REM git add marlo\envs\TreasurehuntTrain4\templates\mission.xml
-git add marlo\envs\TreasurehuntTrain5\__init__.py  marlo\envs\TreasurehuntTrain5\main.py   
-REM git add marlo\envs\TreasurehuntTrain5\templates\mission.xml
+REM git add marlo\envs\TreasurehuntTrain1\__init__.py  marlo\envs\TreasurehuntTrain1\main.py   
+git add marlo\envs\TreasurehuntTrain1\templates\mission.xml
+REM git add marlo\envs\TreasurehuntTrain2\__init__.py  marlo\envs\TreasurehuntTrain2\main.py   
+git add marlo\envs\TreasurehuntTrain2\templates\mission.xml
+REM git add marlo\envs\TreasurehuntTrain3\__init__.py  marlo\envs\TreasurehuntTrain3\main.py   
+git add marlo\envs\TreasurehuntTrain3\templates\mission.xml
+REM git add marlo\envs\TreasurehuntTrain4\__init__.py  marlo\envs\TreasurehuntTrain4\main.py   
+git add marlo\envs\TreasurehuntTrain4\templates\mission.xml
+REM git add marlo\envs\TreasurehuntTrain5\__init__.py  marlo\envs\TreasurehuntTrain5\main.py   
+git add marlo\envs\TreasurehuntTrain5\templates\mission.xml
