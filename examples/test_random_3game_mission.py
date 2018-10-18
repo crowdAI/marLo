f70d12c9feb811ba93668822ecde203197936af3
AndKram <v-andkra@microsoft.com>
2018-10-18 17:08:21 +0100

    tidy

--- examples/test_random_3game_mission.py
+++ examples/test_random_3game_mission.py
@@ -6,19 +6,17 @@ import random
 import argparse
 import itertools
 
-print("testing 123")
-
 parser = argparse.ArgumentParser(description='random test marlo comp xml')
 parser.add_argument('--episodes', type=int, default=10, help='number of episodes')
 args = parser.parse_args()
 
-missionnames = ("BuildbattleTrain", "MobchaseTrain", "TreasurehuntTrain")
-missions = [x for x in itertools.chain.from_iterable([[missionnames[0] + str(i), missionnames[1] + str(i), missionnames[2] + str(i)] for i in range(1, 6)])]
-
+games = ("BuildbattleTrain", "MobchaseTrain", "TreasurehuntTrain")
+missions = [x for x in itertools.chain.from_iterable([[games[0] + str(i),
+                                                       games[1] + str(i),
+                                                       games[2] + str(i)] for i in range(1, 6)])]
 print(missions)
 
 client_pool = [('127.0.0.1', 10000), ('127.0.0.1', 10001)]
-
 all_join_tokens = [marlo.make('MarLo-' + mission + '-v0',
                               params={
                                   "client_pool": client_pool,
@@ -30,12 +28,12 @@ all_join_tokens = [marlo.make('MarLo-' + mission + '-v0',
                                   "comp_all_commands": ["move", "turn", "use"]
                               }) for mission in missions]
 
+
 @marlo.threaded
-def run_agent(join_token):
-    env = marlo.init(join_token)
-    frame = env.reset()
+def run_agent(token):
+    env = marlo.init(token)
+    env.reset()
     done = False
-    count = 0
     while not done:
         _action = env.action_space.sample()
         obs, reward, done, info = env.step(_action)
@@ -47,12 +45,10 @@ def run_agent(join_token):
 
 
 episodes = 0
-
 while episodes < args.episodes:
     episodes += 1
     join_tokens = random.choice(all_join_tokens)
     threads = [run_agent(join_token)[0] for join_token in join_tokens]
     [t.join() for t in threads]
-
 
 
