0ae213e563e6b5fe4bee3fceeca5a1d810dc6e47
AndKram <v-andkra@microsoft.com>
2018-11-09 10:23:51 +0000

    set_log_dir2

--- marlo/experiments/__init__.py
+++ marlo/experiments/__init__.py
@@ -2,3 +2,4 @@ from marlo.experiments.evaluator import eval_performance  # NOQA
 
 from marlo.experiments.train_agent import train_agent  # NOQA
 from marlo.experiments.train_agent import train_agent_with_evaluation  # NOQA
+from marlo.experiments.train_agent import set_log_base_dir, get_log_base_dir  # NOOA
